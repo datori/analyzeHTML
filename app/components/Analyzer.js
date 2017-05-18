a3b92cb9e68c185c38eec5ee105ddb3d06740607
Daniel Serratore <daniel75027@gmail.com>
2017-05-18 16:09:13 -0500

    Integrated components, added chart.js dependancy, added chart.js chart for HTML visualization.

--- app/components/Analyzer.js
+++ app/components/Analyzer.js
@@ -1,14 +1,16 @@
 var React = require("react");
-var TreeVisual = require("./TreeVisual");
+var Chart = require('chart.js');
 
 class Analyzer extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			HTML: "<div></div>"
+			HTML: ""
 		}
 
+		// Make sure methods are always called with correct context.
 		this.updateHTML = this.updateHTML.bind(this);
+		this.renderGraph = this.renderGraph.bind(this);
 	}
 
 	updateHTML(){
@@ -17,16 +19,60 @@ class Analyzer extends React.Component{
 				HTML: document.getElementById("htmlInput").value
 			}
 		});
+		this.renderGraph();
+	}
+
+	renderGraph(){
+		// Generate array of height points of HTML.
+		var tags = this.state.HTML.match(/<.*?>/gi);
+		var height = 0;
+		var height_points = [0];
+		tags.forEach(function(tag){
+			if (tag.match(/<\/.*>/i)){
+				height--;
+			}
+			else{
+				height++;
+			}
+			height_points.push(height);
+		});
+		// Canvas
+		var ctx = "dataChart";
+		var dataChart = new Chart(ctx, {
+		    type: 'line',
+		    data: {
+		    	// Each data point must have a corresponding label, so we generate an array of empty labels for the length of the data set.
+		        labels: height_points.map(() => {return ""}),
+		        datasets: [{
+		            label: 'HTML Layers',
+		            data: height_points,
+		            lineTension: 0.1
+		           
+		        }]
+		    },
+		    options: {
+		    	responsive: true,
+		        scales: {
+		            yAxes: [{
+		                ticks: {
+		                    beginAtZero:true
+		                }
+		            }]
+		        }
+		    }
+		});
+
 	}
 
 	render(){
 		return(
 			<div className="container">
-				<TreeVisual HTML_Data={this.state.HTML} />
+				<div className="visualization">
+					<canvas id="dataChart"></canvas>
+				</div>
 				<div className="input-group">
 					<textarea id="htmlInput"></textarea>
 					<button onClick={this.updateHTML}>Analyze</button>
-
 				</div>
 			</div>
 		)
