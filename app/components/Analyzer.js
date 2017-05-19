3ba38fea10e2f65d7e107dd7074e81e425628721
Daniel Serratore <daniel75027@gmail.com>
2017-05-19 03:23:08 -0500

    Fixed multiple render graph bug, restructured Analyzer to make use of React component lifecycle, added void tag support, added comments.

--- app/components/Analyzer.js
+++ app/components/Analyzer.js
@@ -5,49 +5,12 @@ class Analyzer extends React.Component{
 	constructor(props){
 		super(props);
 
-		// Make sure methods are always called with correct context.
+		this.state = {
+			graph: false
+		}
+
+		// Make sure method is always called with correct context.
 		this.renderGraph = this.renderGraph.bind(this);
-	}
-
-	renderGraph(){
-		// Generate array of height points of HTML.
-		var tags = document.getElementById("htmlInput").value.match(/<.*?>/gi);
-		var height = 0;
-		var height_points = [0];
-		tags.forEach(function(tag){
-			if (tag.match(/<\/.*>/i)){
-				height--;
-			}
-			else{
-				height++;
-			}
-			height_points.push(height);
-		});
-		// Canvas
-		var dataChart = new Chart("dataChart", {
-		    type: 'line',
-		    data: {
-		    	// Each data point must have a corresponding label, so we generate an array of empty labels for the length of the data set.
-		        labels: height_points.map(() => {return ""}),
-		        datasets: [{
-		            label: 'HTML Layers',
-		            data: height_points,
-		            lineTension: 0.2
-		           
-		        }]
-		    },
-		    options: {
-		    	responsive: true,
-		        scales: {
-		            yAxes: [{
-		                ticks: {
-		                    beginAtZero:true
-		                }
-		            }]
-		        }
-		    }
-		});
-
 	}
 
 	render(){
@@ -62,6 +25,62 @@ class Analyzer extends React.Component{
 				</div>
 			</div>
 		)
+	}
+
+	componentDidMount(){
+		var graph = new Chart("dataChart", {
+		    type: 'line',
+		    data: {
+		        labels: [],
+		        datasets: [{
+		            label: 'HTML Layers',
+		            data: [],
+		            lineTension: 0.2
+		        }]
+		    },
+		    options: {
+		    	responsive: true,
+		        scales: {
+		            yAxes: [{
+		                ticks: {
+		                    beginAtZero: true,
+		                    fixedStepSize: 1
+		                },
+		                scaleLabel:{
+		                	display: true,
+		                	labelString: "Layers"
+		                }
+		            }]
+		        }
+		    }
+		});
+		this.setState(() => { return {graph: graph} } );
+	}
+
+	renderGraph(){
+		// Generate array of height points of HTML.
+		var tags = document.getElementById("htmlInput").value.match(/<.*?>/gi);
+		var height = 0;
+		var height_points = [0];
+		tags.forEach(function(tag){
+			if (tag.match(/<(area|base|br|col|embed|hr|img|input|link|meta|param|source|track|wbr)+.*>/i)){
+				// Keep same height for void tags.
+			}
+			else if (tag.match(/<\/.*>/i)){
+				// Decrement height for closing tags.
+				height--;
+			}
+			else{
+				// Increment height for opening tags.
+				height++;
+			}
+			height_points.push(height);
+		});
+		// Update canvas.
+		// Each data point must have a corresponding label, so we generate an array of empty labels for the length of the data set.
+		this.state.graph.data.labels = height_points.map(() => {return ""});
+		this.state.graph.data.datasets[0].data = height_points;
+		this.state.graph.update();
 	}
 }
 
